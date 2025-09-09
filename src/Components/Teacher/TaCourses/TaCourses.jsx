3ec3125b69ad4b82cf57a24110b043b9308652bb
akbaruddin678 <akbaruddin678@gmail.com>
2025-09-09 10:35:44 +0500

    base url

--- src/Components/Teacher/TaCourses/TaCourses.jsx
+++ src/Components/Teacher/TaCourses/TaCourses.jsx
@@ -12,7 +12,9 @@ import {
   MdSave,
 } from "react-icons/md";
 // const API_BASE ="http://localhost:5000/api/v1";
-const API_BASE = import.meta.env?.VITE_API_BASE || "/api/v1";
+const API_BASE = "/api/v1";
+
+
 const token = () => localStorage.getItem("token");
 
 const TYPE_OPTIONS = [
